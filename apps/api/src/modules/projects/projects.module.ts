01a17b25d8e07e8cde123f36086d3496a3a88f8f
tabanbianca-ai <tabanbianca@gmail.com>
2026-06-16 10:41:33 +0200

    Update projects.module.ts

--- apps/api/src/modules/projects/projects.module.ts
+++ apps/api/src/modules/projects/projects.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { runtimeDatabaseProvider } from "../runtime-database.provider";
 import { ProjectsController } from "./projects.controller";
 import { DatabaseProjectsRepository } from "./projects.repository";
 import { ProjectsService } from "./projects.service";
 
 @Module({
   controllers: [ProjectsController],
-  providers: [DatabaseProjectsRepository, ProjectsService],
+  providers: [runtimeDatabaseProvider, DatabaseProjectsRepository, ProjectsService],
   exports: [ProjectsService]
 })
 export class ProjectsModule {}
