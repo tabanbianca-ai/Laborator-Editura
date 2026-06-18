b0a1c85575c1dbf055655f8949f1c453ea9c028a
tabanbianca-ai <tabanbianca@gmail.com>
2026-06-18 14:30:54 +0200

    Update lexicographic.module.ts

--- apps/api/src/modules/lexicographic/lexicographic.module.ts
+++ apps/api/src/modules/lexicographic/lexicographic.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { runtimeDatabaseProvider } from "../runtime-database.provider";
 import { LexicographicController } from "./lexicographic.controller";
-import { InMemoryLexicographicRepository } from "./lexicographic.repository";
+import { DatabaseLexicographicRepository } from "./lexicographic.repository";
 import { LexicographicService } from "./lexicographic.service";
 
 @Module({
   controllers: [LexicographicController],
-  providers: [InMemoryLexicographicRepository, LexicographicService],
+  providers: [runtimeDatabaseProvider, DatabaseLexicographicRepository, LexicographicService],
   exports: [LexicographicService]
 })
 export class LexicographicModule {}
