f3fd2b26d787d13af989509e418634bcea19056a
tabanbianca-ai <tabanbianca@gmail.com>
2026-06-24 09:47:29 +0200

    Create enterprise-admin.controller.ts

---
+++ apps/api/src/modules/enterprise-admin/enterprise-admin.controller.ts
@@ -0,0 +1,68 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { CurrentActor } from "../auth/request-context.decorator";
+import { type AuthenticatedRequestContext } from "../auth/request-context.types";
+import { EnterpriseAdminService } from "./enterprise-admin.service";
+import {
+  type AssignAdminRoleInput,
+  type CreateAdminInvitationInput,
+  type CreateAdminRoleInput,
+  type CreateAdminUserInput
+} from "./enterprise-admin.types";
+
+@Controller("admin")
+export class EnterpriseAdminController {
+  constructor(private readonly enterpriseAdminService: EnterpriseAdminService) {}
+
+  @Get("users")
+  listUsers(@CurrentActor() actor: AuthenticatedRequestContext) {
+    return this.enterpriseAdminService.listUsers(actor);
+  }
+
+  @Post("users")
+  createUser(
+    @CurrentActor() actor: AuthenticatedRequestContext,
+    @Body() input: CreateAdminUserInput
+  ) {
+    return this.enterpriseAdminService.createUser(actor, input);
+  }
+
+  @Get("roles")
+  listRoles(@CurrentActor() actor: AuthenticatedRequestContext) {
+    return this.enterpriseAdminService.listRoles(actor);
+  }
+
+  @Post("roles")
+  createRole(
+    @CurrentActor() actor: AuthenticatedRequestContext,
+    @Body() input: CreateAdminRoleInput
+  ) {
+    return this.enterpriseAdminService.createRole(actor, input);
+  }
+
+  @Get("permissions")
+  listPermissions(@CurrentActor() actor: AuthenticatedRequestContext) {
+    return this.enterpriseAdminService.listPermissions(actor);
+  }
+
+  @Post("users/:id/roles")
+  assignRole(
+    @CurrentActor() actor: AuthenticatedRequestContext,
+    @Param("id") id: string,
+    @Body() input: AssignAdminRoleInput
+  ) {
+    return this.enterpriseAdminService.assignRole(actor, id, input);
+  }
+
+  @Post("invitations")
+  createInvitation(
+    @CurrentActor() actor: AuthenticatedRequestContext,
+    @Body() input: CreateAdminInvitationInput
+  ) {
+    return this.enterpriseAdminService.createInvitation(actor, input);
+  }
+
+  @Get("audit")
+  listAudit(@CurrentActor() actor: AuthenticatedRequestContext) {
+    return this.enterpriseAdminService.listAudit(actor);
+  }
+}
