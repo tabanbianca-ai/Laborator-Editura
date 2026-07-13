94b0829e0a576644ef02b4914d8fd4bd64e6bed7
tabanbianca-ai <tabanbianca@gmail.com>
2026-07-13 14:58:53 +0200

    Update terminology.module.ts

--- apps/api/src/modules/terminology/terminology.module.ts
+++ apps/api/src/modules/terminology/terminology.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { LexicographicModule } from "../lexicographic/lexicographic.module";
+import { runtimeDatabaseProvider } from "../runtime-database.provider";
 import { InMemoryTerminologyRepository } from "./terminology.repository";
 import { TerminologyController } from "./terminology.controller";
 import { TerminologyService } from "./terminology.service";
@@ -7,7 +8,7 @@ import { TerminologyService } from "./terminology.service";
 @Module({
   imports: [LexicographicModule],
   controllers: [TerminologyController],
-  providers: [InMemoryTerminologyRepository, TerminologyService],
+  providers: [runtimeDatabaseProvider, InMemoryTerminologyRepository, TerminologyService],
   exports: [TerminologyService]
 })
 export class TerminologyModule {}
