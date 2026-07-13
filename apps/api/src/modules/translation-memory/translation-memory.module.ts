f156140d41b94027a61cab2a9cbfc61352781cef
tabanbianca-ai <tabanbianca@gmail.com>
2026-07-13 14:59:06 +0200

    Update translation-memory.module.ts

--- apps/api/src/modules/translation-memory/translation-memory.module.ts
+++ apps/api/src/modules/translation-memory/translation-memory.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { runtimeDatabaseProvider } from "../runtime-database.provider";
 import { TranslationMemoryController } from "./translation-memory.controller";
 import { InMemoryTranslationMemoryRepository } from "./translation-memory.repository";
 import { TranslationMemoryService } from "./translation-memory.service";
 
 @Module({
   controllers: [TranslationMemoryController],
-  providers: [InMemoryTranslationMemoryRepository, TranslationMemoryService],
+  providers: [runtimeDatabaseProvider, InMemoryTranslationMemoryRepository, TranslationMemoryService],
   exports: [TranslationMemoryService]
 })
 export class TranslationMemoryModule {}
