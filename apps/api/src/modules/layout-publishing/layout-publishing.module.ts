5cb7762f0e286863b2e34c01f24fe017bdbfbf26
tabanbianca-ai <tabanbianca@gmail.com>
2026-07-20 14:18:17 +0200

    Update layout-publishing.module.ts

--- apps/api/src/modules/layout-publishing/layout-publishing.module.ts
+++ apps/api/src/modules/layout-publishing/layout-publishing.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
+import { ExportModule } from "../export/export.module";
+import { LibraryModule } from "../library/library.module";
+import { RightsProvenanceModule } from "../rights-provenance/rights-provenance.module";
 import { runtimeDatabaseProvider } from "../runtime-database.provider";
+import { WorkflowModule } from "../workflow/workflow.module";
 import { LayoutPublishingController } from "./layout-publishing.controller";
 import { DatabaseLayoutPublicationRepository } from "./layout-publishing.repository";
 import { LayoutPublishingService } from "./layout-publishing.service";
 
 @Module({
+  imports: [
+    ExportModule,
+    LibraryModule,
+    RightsProvenanceModule,
+    WorkflowModule
+  ],
   controllers: [LayoutPublishingController],
   providers: [
     runtimeDatabaseProvider,
